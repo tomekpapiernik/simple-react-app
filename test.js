474105446e304b55f9cc6b871c732279a2eccba8
Tom Papiernik <4kmt22013@gmail.com>
2021-11-12 14:07:53 +0100

    update test

--- test.js
+++ test.js
@@ -1,18 +1,17 @@
 import http from "k6/http";
-
+ 
 export let options = {
-    thresholds: {
-      "http_req_duration": ["p(95)<90"],
-      "http_req_failed": ["rate<0.01"],
-      "iteration_duration": ["p(95)<80"],
+   thresholds: {
+     "http_req_duration": ["p(95)<1000"],
+     "http_req_failed": ["rate<0.01"]
     },
-    stages: [
-        { duration: '15s', target: 50 },
-        { duration: '30s', target: 50 },
-        { duration: '15s', target: 0 },
-      ],
+   stages: [
+       { duration: '15s', target: 50 },
+       { duration: '30s', target: 50 },
+       { duration: '15s', target: 0 },
+     ],
 };
-
+ 
 export default function() {
-    http.get('http://159.223.29.170/8080')
-  };
+   http.get('{YOUR_STAGING_ENV_URL}')
+ };
